02aa5060acdd8fb38f8b624aec5e36545f0cd571
Json <jasonplatino798@gmail.com>
2025-06-19 17:35:01 +0800

    test confirmation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // lib/main.dart
+//testing for git commit <-----
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
